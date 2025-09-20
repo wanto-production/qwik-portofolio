c168ec2494e5c5de5daac16e3700ed793f76d99d
wanto-production <ikwansatria3974@gmail.com>
2025-09-20 11:07:54 +0700

    initial commit

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -15,6 +15,7 @@ export const RouterHead = component$(() => {
       <link rel="canonical" href={loc.url.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      <meta name="google-site-verification" content="bWRaoFVlyB345Wietszb9IvK4x9MIN9hvtEA9ChIgoQ" />
 
       {head.meta.map((m) => (
         <meta key={m.key} {...m} />
